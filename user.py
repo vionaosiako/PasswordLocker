691e4c127545a2fbb99847df3ee098692f4d01e7
vionaosiako <viona.osiako@student.morigaschool.com>
2022-04-23 11:56:08 +0300

    create new instances of a class user

--- user.py
+++ user.py
@@ -3,4 +3,12 @@ class User:
     Class that generates new instances of user
     """
 
-    pass
+    def __init__(self,first_name,last_name,username,password):
+
+        '''
+        __init__ method that helps us define properties for our objects.
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
+        self.password = password
