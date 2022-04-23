24b5a6bd9d72ed3ab16c4dabe62214da5edce73a
vionaosiako <viona.osiako@student.morigaschool.com>
2022-04-23 11:58:53 +0300

    Empty user list

--- user.py
+++ user.py
@@ -2,6 +2,8 @@ class User:
     """
     Class that generates new instances of user
     """
+    
+    user_list = [] # Empty user list
 
     def __init__(self,first_name,last_name,username,password):
 
