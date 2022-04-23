7d344b32740a6815b547086f0bc613c791dd0571
vionaosiako <viona.osiako@student.morigaschool.com>
2022-04-23 18:19:42 +0300

    create a user_contact method

--- user.py
+++ user.py
@@ -14,3 +14,13 @@ class User:
         self.last_name = last_name
         self.username = username
         self.password = password
+        
+    user_list = [] # Empty user list
+    # Init method up here
+    def save_user(self):
+
+        '''
+        save_user method saves user objects into user_list
+        '''
+
+        User.user_list.append(self)
--- user_test.py
+++ user_test.py
@@ -25,6 +25,15 @@ class TestContact(unittest.TestCase):
         self.assertEqual(self.new_user.last_name,"Osiako")
         self.assertEqual(self.new_user.username,"Lioness")
         self.assertEqual(self.new_user.password,"Lioness")
+    
+    #second test   
+    def test_save_user(self):
+        '''
+        test_save_user test case to test if the user object is saved into
+        the user list
+        '''
+        self.new_user.save_user() # saving the new user
+        self.assertEqual(len(User.user_list),1)
 
 
 if __name__ == '__main__':
